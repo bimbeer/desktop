bde6789cce57fbbfd6cd9e60b911b8d57b6cefd9
Zayooo00 <mrcold135@gmail.com>
2023-04-05 14:23:36 +0200

    updated dashboard to display suggested profiles

--- src/renderer/pages/Dashboard.jsx
+++ src/renderer/pages/Dashboard.jsx
@@ -1,6 +1,8 @@
+/* eslint-disable no-restricted-syntax */
 import React, { useEffect, useState } from 'react';
 import BimbeerCard from 'renderer/components/BimbeerCard.jsx';
 import { getUserFromLocalStorage } from 'renderer/context/AuthContext';
+import { Center, Spinner, Text, Flex, Card } from '@chakra-ui/react';
 import {
   collection,
   query,
@@ -12,65 +14,134 @@ import {
 import Sidebar from '../components/Sidebar';
 import { db } from '../firebase/firebase';
 
-export default function Profile() {
+export default function Dashboard() {
   const [profileData, setProfileData] = useState();
+  const [users, setUsers] = useState([]);
+  const [currentUserIndex, setCurrentUserIndex] = useState(0);
+  const [isCardLoading, setIsCardLoading] = useState(true);
+  let content;
 
   useEffect(() => {
     const userId = getUserFromLocalStorage();
     const docRef = doc(db, 'profile', userId);
 
     async function fetchData() {
+      setIsCardLoading(true);
       const docSnap = await getDoc(docRef);
       if (docSnap.exists()) {
         setProfileData(docSnap.data());
       }
+      setIsCardLoading(false);
     }
     fetchData();
   }, []);
 
-  async function getUsersWithMatchingBeersAndInterests() {
-    const userId = getUserFromLocalStorage();
-    let users = [];
+  useEffect(() => {
+    async function getUsersWithMatchingBeersAndInterests() {
+      setIsCardLoading(true);
+      const userId = getUserFromLocalStorage();
+      let matchedUsers = [];
 
-    const genders = ['Man', 'Woman', 'Other'];
-    const interests = [profileData.gender, 'All'];
+      const genders = ['Man', 'Woman', 'Other'];
+      const interests = [profileData.gender, 'All'];
 
-    for (const gender of genders) {
-      for (const interest of interests) {
-        let q;
-        if (profileData.interest === 'All') {
-          q = query(
-            collection(db, 'profile'),
-            where('beers', 'array-contains-any', profileData.beers),
-            where('gender', '==', gender),
-            where('__name__', '!=', userId),
-            where('interest', '==', interest)
-          );
-        } else {
-          q = query(
-            collection(db, 'profile'),
-            where('beers', 'array-contains-any', profileData.beers),
-            where('interest', '==', profileData.gender),
-            where('gender', '==', profileData.interest),
-            where('__name__', '!=', userId)
-          );
+      async function getMatchedUsers(gender, interest) {
+        const beerChunks = [];
+        for (let i = 0; i < profileData.beers.length; i += 10) {
+          beerChunks.push(profileData.beers.slice(i, i + 10));
         }
-        const querySnapshot = await getDocs(q);
-        querySnapshot.forEach((document) => {
-          users.push(document.data());
+
+        const promises = beerChunks.map(async (beers) => {
+          let q;
+          if (profileData.interest === 'All') {
+            q = query(
+              collection(db, 'profile'),
+              where('beers', 'array-contains-any', beers),
+              where('gender', '==', gender),
+              where('__name__', '!=', userId),
+              where('interest', '==', interest)
+            );
+          } else {
+            q = query(
+              collection(db, 'profile'),
+              where('beers', 'array-contains-any', beers),
+              where('interest', '==', profileData.gender),
+              where('gender', '==', profileData.interest),
+              where('__name__', '!=', userId)
+            );
+          }
+          const querySnapshot = await getDocs(q);
+          querySnapshot.forEach((document) => {
+            matchedUsers.push(document.data());
+          });
         });
+
+        await Promise.all(promises);
       }
+
+      const promises = [];
+      for (const gender of genders) {
+        for (const interest of interests) {
+          promises.push(getMatchedUsers(gender, interest));
+        }
+      }
+      await Promise.all(promises);
+
+      matchedUsers = [...new Set(matchedUsers)];
+      setUsers(matchedUsers);
+      setIsCardLoading(false);
     }
-    users = [...new Set(users)];
-    console.log(profileData.interest);
-    console.log(users);
+
+    if (profileData) {
+      getUsersWithMatchingBeersAndInterests();
+    }
+  }, [profileData]);
+
+  const handleUserAction = () => {
+    if (currentUserIndex < users.length - 1) {
+      setCurrentUserIndex((prevIndex) => prevIndex + 1);
+    }
+  };
+
+  if (isCardLoading) {
+    content = (
+      <Center h="100vh">
+        <Spinner
+          thickness="4px"
+          speed="0.65s"
+          emptyColor="gray.700"
+          color="white"
+          size="xl"
+        />
+      </Center>
+    );
+  } else if (users.length > 0) {
+    content = (
+      <BimbeerCard
+        key={users[currentUserIndex].username}
+        user={users[currentUserIndex]}
+        onUserAction={handleUserAction}
+      />
+    );
+  } else {
+    content = (
+      <Flex minH="100vh" align="center" justify="center" ml="100px" mr="20px">
+        <Card p={10} bg="whiteAlpha.100" maxW={650}>
+          <Center>
+            <Text color="white">
+              It seems like we have no suggestions ready for you at the moment,
+              consider enabling global search or wait for new people!
+            </Text>
+          </Center>
+        </Card>
+      </Flex>
+    );
   }
 
   return (
     <>
       <Sidebar />
-      <button onClick={getUsersWithMatchingBeersAndInterests}> EEEEEEEEEEEEEEE </button>
-      <BimbeerCard />
+      {content}
     </>
   );
 }
