a0c3198a0e61da41889b5605f9de6902c4d3fac5
Zayooo00 <mrcold135@gmail.com>
2023-04-12 23:30:50 +0200

    avoided props spreading

--- src/renderer/components/Profile/ProfileCards/Card.js
+++ src/renderer/components/Profile/ProfileCards/Card.js
@@ -1,14 +1,28 @@
 import { Box, useStyleConfig } from '@chakra-ui/react';
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function Card(props) {
+export default function Card(props) {
   const { variant, children, ...rest } = props;
   const styles = useStyleConfig('Card', { variant });
   return (
-    <Box __css={styles} {...rest}>
+    <Box
+      __css={styles}
+      p={rest.p}
+      bg={rest.bg}
+      rounded={rest.rounded}
+      my={rest.my}
+    >
       {children}
     </Box>
   );
 }
 
-export default Card;
+Card.propTypes = {
+  variant: PropTypes.string,
+  children: PropTypes.node.isRequired,
+};
+
+Card.defaultProps = {
+  variant: 'defaultVariant',
+};
--- src/renderer/components/Profile/ProfileCards/CardBody.js
+++ src/renderer/components/Profile/ProfileCards/CardBody.js
@@ -1,15 +1,23 @@
 import { Box, useStyleConfig } from '@chakra-ui/react';
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function CardBody(props) {
+export default function CardBody(props) {
   const { variant, children, ...rest } = props;
   const styles = useStyleConfig('CardBody', { variant });
   // Pass the computed styles into the `__css` prop
   return (
-    <Box __css={styles} {...rest}>
+    <Box __css={styles} px={rest.px}>
       {children}
     </Box>
   );
 }
 
-export default CardBody;
+CardBody.propTypes = {
+  variant: PropTypes.string,
+  children: PropTypes.node.isRequired,
+};
+
+CardBody.defaultProps = {
+  variant: 'defaultVariant',
+};
